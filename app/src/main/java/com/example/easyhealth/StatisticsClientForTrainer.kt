3e1f1acc099b5ff2789ce90a29678a8adc90a938
ArnolSwet <A.1r.2n.3i.4>
2020-03-16 14:20:52 +0100

    Adding Graphs

--- app/src/main/java/com/example/easyhealth/StatisticsClientForTrainer.kt
+++ app/src/main/java/com/example/easyhealth/StatisticsClientForTrainer.kt
@@ -2,11 +2,19 @@ package com.example.easyhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 
 class StatisticsClientForTrainer : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics_client_for_trainer)
+        val monthGraph = findViewById(R.id.monthGraph) as GraphView
+
+        val series1 = LineGraphSeries(arrayOf(DataPoint(0,1),DataPoint(1,5),DataPoint(2,3)))
+        monthGraph.addSeries(series1)
     }
 }
