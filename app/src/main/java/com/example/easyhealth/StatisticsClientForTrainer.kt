de51887ae7f4064c8b6aa60abf91820723ba3695
ArnolSwet <A.1r.2n.3i.4>
2020-03-16 17:23:36 +0100

    Statistics Client for Trainer Finished

--- app/src/main/java/com/example/easyhealth/StatisticsClientForTrainer.kt
+++ app/src/main/java/com/example/easyhealth/StatisticsClientForTrainer.kt
@@ -12,9 +12,5 @@ class StatisticsClientForTrainer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics_client_for_trainer)
-        val monthGraph = findViewById(R.id.monthGraph) as GraphView
-
-        val series1 = LineGraphSeries(arrayOf(DataPoint(0,1),DataPoint(1,5),DataPoint(2,3)))
-        monthGraph.addSeries(series1)
     }
 }
