e663dec062b005b4e6cd646b5b2459e707476d03
ArnolSwet <A.1r.2n.3i.4>
2020-03-16 13:42:34 +0100

    Init StatisticsClientForTrainer

--- app/src/main/java/com/example/easyhealth/StatisticsClientForTrainer.kt
+++ app/src/main/java/com/example/easyhealth/StatisticsClientForTrainer.kt
@@ -3,10 +3,10 @@ package com.example.easyhealth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StatisticsClientForTrainer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_statistics_client_for_trainer)
     }
 }
