f5609313a3ab3e9ac0d6685e2b355731d15abbdd
ArnolSwet <A.1r.2n.3i.4>
2020-03-19 00:35:25 +0100

    Change name statistics

---
+++ app/src/main/java/com/example/easyhealth/Statistics.kt
@@ -0,0 +1,13 @@
+package com.example.easyhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Statistics : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+    }
+}
