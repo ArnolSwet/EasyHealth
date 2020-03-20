275d68e181f105627ecdb55be6824c5af199c4f2
ignnasi <ignnasi.ranz@gmail.com>
2020-03-20 20:51:00 +0100

    Chat layout WIP

---
+++ app/src/main/java/com/example/easyhealth/ChatWithTrainer.kt
@@ -0,0 +1,11 @@
+package com.example.easyhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatWithTrainer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_with_trainer)
+    }
+}
