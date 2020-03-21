ddf2ecdaefbca9eba1d3073915dc7745fc18e5f1
joelbolwijn <joel.bolwijn@gmail.com>
2020-03-21 21:57:36 +0100

    wip joel primer ui la chunga

--- app/src/main/java/com/example/easyhealth/ClientForTrainer.kt
+++ app/src/main/java/com/example/easyhealth/ClientForTrainer.kt
@@ -7,6 +7,6 @@ class ClientForTrainer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_make_adiet)
+        setContentView(R.layout.activity_client_for_trainer)
     }
 }
