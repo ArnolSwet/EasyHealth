2bc6cccbec57a87259d550fbd50fbf883fa78c66
ignnasi <ignnasi.ranz@gmail.com>
2020-03-22 19:38:27 +0100

    Changing things

--- app/src/main/java/com/example/easyhealth/PersonalDiet.kt
+++ app/src/main/java/com/example/easyhealth/PersonalDiet.kt
@@ -1,12 +1,19 @@
 package com.example.easyhealth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PersonalDiet : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_diet)
+    }
+
+    fun mainClient(view: View){
+        val intent = Intent(this, activity_main_client::class.java)
+        startActivity(intent)
     }
 }
