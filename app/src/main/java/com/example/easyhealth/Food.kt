f3978c675f88561e681401e0dd7c3608289357a0
ArnolSwet <A.1r.2n.3i.4>
2020-03-31 21:03:29 +0200

    Class Food Finished

--- app/src/main/java/com/example/easyhealth/Food.kt
+++ app/src/main/java/com/example/easyhealth/Food.kt
@@ -2,6 +2,38 @@ package com.example.easyhealth
 
 import android.media.Image
 
-class Food (var name: String, var calories: Float, var image: Image) {
+class Food constructor(private var name: String, private var calories: Float, private var image: Image, private var tipus: String) {
+
+    fun setName(name: String) {
+        this.name = name
+    }
+
+    fun getName(): String {
+        return this.name
+    }
+
+    fun setCalories(calories: Float) {
+        this.calories = calories
+    }
+
+    fun getCalories(): Float {
+        return this.calories
+    }
+
+    fun setImage(image: Image) {
+        this.image = image
+    }
+
+    fun getImage(): Image {
+        return this.image
+    }
+
+    fun setTipus(tipus: String) {
+        this.tipus = tipus
+    }
+
+    fun getTipus(): String {
+        return this.tipus
+    }
 
 }
