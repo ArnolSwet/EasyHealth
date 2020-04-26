9d0412e234f55ca250811e0b96266cf9351c16b4
ArnolSwet <31952228+arnolswet@users.noreply.github.com>
2020-04-26 23:28:13 +0200

    Read data on MainClient

--- app/src/main/java/appEasyHealth/Client/MainClient.kt
+++ app/src/main/java/appEasyHealth/Client/MainClient.kt
@@ -3,14 +3,73 @@ package com.example.appEasyHealth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import com.example.easyhealth.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 
 class MainClient : AppCompatActivity() {
 
+    private lateinit var database: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var name:  TextView
+    private lateinit var suscription: TextView
+    private lateinit var weight: TextView
+    private lateinit var height: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database.getReference("User")
+        auth = FirebaseAuth.getInstance()
+        val user:FirebaseUser? = auth.currentUser
+        val userdB = databaseReference.child(user?.uid!!)
+        val nameClient = userdB.child("Name")
+        val weightClient = userdB.child("Weight")
+        val heightClient = userdB.child("Height")
         setContentView(R.layout.activity_main_client)
+        name = findViewById(R.id.txtClientName)
+        suscription = findViewById(R.id.txtCliSubscriptionNum)
+        weight = findViewById(R.id.txtCliWeightNum)
+        height = findViewById(R.id.txtCliHeightNum)
+        nameClient.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(applicationContext,"Fail to read data", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.value != null) {
+                    name.text = p0.value.toString()
+                }
+            }
+        })
+        weightClient.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(applicationContext,"Fail to read data", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.value != null) weight.text = p0.value.toString()
+
+            }
+        })
+        heightClient.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(applicationContext,"Fail to read data", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.value != null)height.text = p0.value.toString()
+            }
+        })
     }
 
     fun goback(view: View) {
